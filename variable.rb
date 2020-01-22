4670b4cb6106ececddbee5919355f458dcad018c
Yani Moore <yanimoore09@gmail.com>
2020-01-22 16:16:28 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
-greeting = ("hello world")
-puts greeting
+greeting = "hello World"
+puts "#{greeting}"
